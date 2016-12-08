ed5338c21627b8cb11acba5bf02ff78ed13b761b
bruce <brucezeng@alvose.com>
2016-12-08 18:23:26 +0800

    Add permission request for api level 23

--- app/src/main/java/com/logdown/mycodetub/updatelocationtest/MainActivity.java
+++ app/src/main/java/com/logdown/mycodetub/updatelocationtest/MainActivity.java
@@ -1,14 +1,20 @@
 package com.logdown.mycodetub.updatelocationtest;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.location.LocationListener;
+
+import static android.Manifest.permission.*;
 
 public class MainActivity extends Activity implements LocationListener {
 
@@ -17,20 +23,64 @@ public class MainActivity extends Activity implements LocationListener {
     private TextView mTextView;
     private int count = 1;
 
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private boolean mLocationPermitGranted = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initButtons();
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        }else {
+
+            mLocationPermitGranted = true;
+        }
 
         mGmsLocationUtil = new GmsLocationUtil(this);
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermitGranted = true;
+//                    mGmsLocationUtil.connect();
+//                    if (mGmsLocationUtil.isConnected() && mGmsLocationUtil.isRequestLocationUpdates()) {
+//                        startUpdateLocation();
+//                    }
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+                    mLocationPermitGranted = false;
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
     private void initButtons() {
         Button startUpdateLocationBtn = (Button) findViewById(R.id.start_update_location_btn);
         startUpdateLocationBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(mLocationPermitGranted) {
+                    mGmsLocationUtil.connect();
+                    if (mGmsLocationUtil.isConnected() && mGmsLocationUtil.isRequestLocationUpdates()) {
+                        startUpdateLocation();
+                    }
+                }
                 startUpdateLocation();
             }
         });
@@ -57,27 +107,33 @@ public class MainActivity extends Activity implements LocationListener {
     @Override
     protected void onStart() {
         super.onStart();
-        mGmsLocationUtil.connect();
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (mGmsLocationUtil.isConnected() && mGmsLocationUtil.isRequestLocationUpdates()) {
-            startUpdateLocation();
+        if(mLocationPermitGranted) {
+            if (mGmsLocationUtil.isConnected() && mGmsLocationUtil.isRequestLocationUpdates()) {
+                startUpdateLocation();
+            }
         }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        stopUpdateLocation();
+        if(mLocationPermitGranted) {
+            stopUpdateLocation();
+        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mGmsLocationUtil.disConnect();
+        if(mLocationPermitGranted) {
+            mGmsLocationUtil.disConnect();
+        }
     }
 
     @Override
